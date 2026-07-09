975c8e62f8d5bfa16f31650138adb8a07605b6b3
hardBoxzy <89336980+hardBoxzy@users.noreply.github.com>
2026-07-09 23:29:38 +0900

    課題提出 継承を使って自分と家族を紹介しよう

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,23 @@
+package text.section_18.kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		
+		KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();		
+		katoTaro.setGivenName();
+		katoTaro.execIntroduce();
+		
+		
+		
+		KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();		
+		katoIchiro.setGivenName();
+		katoIchiro.execIntroduce();
+		
+		
+		
+		KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();		
+		katoHanako.setGivenName();
+		katoHanako.execIntroduce();
+		
+	}
+}
